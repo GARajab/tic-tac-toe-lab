f6cdb756867e078ea2a7e42d8892c921ebdbc226
GARajab <mohamed.rajab@ewa.bh>
2024-10-04 03:49:49 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,194 @@
+/*-------------------------------- Constants --------------------------------*/
+// this is the winning combos
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [0, 4, 8],
+]
+/*---------------------------- Variables (state) ----------------------------*/
+let board = ["", "", "", "", "", "", "", "", ""]
+let turn = "X"
+let winner = false
+let tie = false
+let currentPlayer = "X"
+let xWinCount = 0
+let oWinCount = 0
+/*------------------------ Cached Element References ------------------------*/
+const bodyModified = document.querySelector("body")
+const toggleButton = document.getElementById("toggle-theme")
+const body = document.body
+const squareEls = document.querySelectorAll(".sqr")
+const messageEl = document.querySelector("#message")
+const boards = document.querySelectorAll(".board")
+// const resetBtnEl = document.querySelector("#reset")
+const animatedReset = document.getElementById("reset")
+const xScoreVal = document.getElementById("xScore")
+const oScoreVal = document.getElementById("oScore")
+const xPlayColor = document.getElementById("plX")
+const oPlayColor = document.getElementById("plO")
+const backgroundImage = document.getElementById("background-image")
+let winningCombination = []
+
+/*-------------------------------- Functions --------------------------------*/
+const init = () => {
+  board = ["", "", "", "", "", "", "", "", ""]
+  turn = "X"
+  winner = false
+  tie = false
+  currentPlayer = "X"
+  rstWinColorBack()
+  showHideResetBtn()
+  render()
+  xPlayColor.style.color = "white"
+  xPlayColor.style.fontWeight = "bold"
+}
+const render = () => {
+  updateBoard()
+  updateMessage()
+}
+const updateBoard = () => {
+  for (let i = 0; i < board.length; i++) {
+    const squareEl = squareEls[i]
+    squareEl.textContent = board[i]
+  }
+}
+
+const updateMessage = () => {
+  if (winner === false && tie === false) {
+    messageEl.innerHTML = ""
+  } else if (winner === false && tie === true) {
+    messageEl.innerHTML = "It Is Tie Please Try Again!"
+    showHideKeepCalm()
+    showHideResetBtn()
+  } else {
+    messageEl.innerHTML = `<img src=/images/fest-bgrmvd.png>  Player ( ${turn} ) Won!  <img src=/images/fest-bgrmvd.png>`
+    showHideResetBtn()
+  }
+}
+
+const handleClick = (event) => {
+  // Obtain the index of the clicked square
+  const squareIndex = parseInt(event.target.id)
+
+  // Check if the square is already taken or the game is over
+  if (board[squareIndex] !== "" || winner) {
+    return
+  }
+
+  // Update the board with the current player's move
+  board[squareIndex] = currentPlayer
+
+  checkForWinner()
+  checkForTie()
+
+  if (winner) {
+    // winnerCounter() // Increment count only if there is a winner now
+  } else {
+    checkForTie()
+    if (!winner) {
+      currentPlayer = currentPlayer === "X" ? "O" : "X" // After checking for winner
+    }
+  }
+
+  updateBoard()
+  placePiece(squareIndex)
+  switchPlayerTurn()
+  updateMessage()
+}
+
+const placePiece = (index) => {
+  board[index] = turn
+  console.log(board)
+}
+
+const checkForWinner = () => {
+  for (let combo of winningCombos) {
+    const [a, b, c] = combo
+    if (board[a] !== "" && board[a] === board[b] && board[a] === board[c]) {
+      winner = true
+      winningCombination = [a, b, c]
+      highlightWinningCombination()
+      winnerCounter()
+      confetti()
+      break
+    }
+  }
+}
+
+const checkForTie = () => {
+  tie = !winner && board.every((square) => square !== "")
+}
+
+const switchPlayerTurn = () => {
+  if (winner) {
+    return
+  } else {
+    if (turn === "X") {
+      turn = "O"
+      oPlayColor.style.color = "#EF476F"
+      xPlayColor.style.color = "white"
+    } else if (turn === "O") {
+      turn = "X"
+      xPlayColor.style.color = "#EF476F"
+      oPlayColor.style.color = "white"
+    }
+  }
+}
+
+const winnerCounter = () => {
+  if (currentPlayer === "X") {
+    xWinCount++
+  } else {
+    oWinCount++
+  }
+
+  oScoreVal.innerHTML = `Score Is: ${oWinCount}`
+  xScoreVal.innerHTML = `Score Is: ${xWinCount}`
+}
+
+const showHideResetBtn = () => {
+  if (animatedReset.style.display === "none") {
+    animatedReset.style.display = ""
+  } else {
+    oPlayColor.style.color = "white"
+    xPlayColor.style.color = "white"
+    animatedReset.style.display = "none"
+  }
+}
+
+const showHideKeepCalm = () => {
+  if (tie) {
+    backgroundImage.classList.add("show")
+    setTimeout("backgroundImage.classList.remove('show')", 3000)
+  } else {
+    return
+  }
+}
+
+const highlightWinningCombination = () => {
+  for (let index of winningCombination) {
+    const cell = document.getElementById(`${index}`)
+    cell.style.backgroundColor = "yellow" // Change this to your desired color
+  }
+}
+
+const rstWinColorBack = () => {
+  for (let i = 0; i < board.length; i++) {
+    const cell = document.getElementById(`${i}`)
+    cell.style.backgroundColor = "" // Reset to default background color
+    cell.textContent = ""
+  }
+}
+/*----------------------------- Event Listeners -----------------------------*/
+// boards.addEventListener("click", (event) => {})
+boards.forEach((cell) => {
+  cell.addEventListener("click", handleClick)
+})
+animatedReset.addEventListener("click", init)
+const setTheme = (theme) => (document.documentElement.className = theme)
+/*----------------------------- random codes-----------------------------*/
